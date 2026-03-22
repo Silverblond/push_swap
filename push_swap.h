f301c47dd59e43a79850421eac7a08b88f21f60a
silverblond <kdg23538@yu.ac.kr>
2026-03-22 17:27:55 +0900

    header

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,28 @@
 /*   By: kdg <kdg@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/22 17:23:23 by kdg               #+#    #+#             */
-/*   Updated: 2026/03/22 17:23:35 by kdg              ###   ########.fr       */
+/*   Updated: 2026/03/22 17:27:38 by kdg              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+typedef struct s_node
+{
+	int				value;
+	int				index;
+	struct s_node	*next;
+}	t_node;
+
+typedef struct s_stack
+{
+	t_node	*top;
+	int		size;
+}	t_stack;
+
+
+#endif
