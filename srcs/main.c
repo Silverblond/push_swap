e540d75fea98ce529f39aa3c2055001e84c58e27
silverblond <kdg23538@yu.ac.kr>
2026-03-23 18:54:49 +0900

    sort5

--- srcs/main.c
+++ srcs/main.c
@@ -6,53 +6,35 @@
 /*   By: kdg <kdg@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/23 02:59:41 by kdg               #+#    #+#             */
-/*   Updated: 2026/03/23 17:47:49 by kdg              ###   ########.fr       */
+/*   Updated: 2026/03/23 18:52:41 by kdg              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
-#include <stdio.h>
 
-static void print_stack(t_stack *s, char *name)
+int	main(int argc, char **argv)
 {
-    t_node  *cur;
+	t_stack *a;
+	t_stack *b;
 
-    printf("=== stack %s (size: %d) ===\n", name, s->size);
-    cur = s->top;
-    while (cur)
-    {
-        printf("[%d] ", cur->value);
-        cur = cur->next;
-    }
-    printf("\n");
+	if (argc < 2)
+		return (0);
+	a = new_stack();
+	b = new_stack();
+	parse(argc, argv, a);
+	set_index(a);
+	if (!is_sorted(a))
+	{
+		if (a->size == 2)
+			sort_2(a);
+		else if (a->size == 3)
+			sort_3(a);
+		else if (a->size <= 5)
+			sort_5(a, b);
+		// else
+		// 	sort_radix(a, b);
+	}
+	free_stack(a);
+	free_stack(b);
+	return (0);
 }
-
-// #include "push_swap.h"
-
-// int main(int argc, char **argv)
-// {
-//     t_stack *a;
-//     t_stack *tmp;
-// 	t_stack *b;
-// 	t_stack *tmp2;
-
-//     a = (t_stack *)malloc(sizeof(t_stack));
-//     a->size=0;
-//     a->top=NULL;
-//     parse(argc, argv, a);
-
-//     tmp = a;
-// 	b = (t_stack *)malloc(sizeof(t_stack));
-// 	b->size=0;
-// 	b->top=NULL;
-// 	tmp2 = b;
-//     while (tmp->top){
-//         printf("val = %d\n", tmp->top->value);
-//         tmp->top = tmp->top->next;
-//     }
-//     while (tmp2->top){
-//         printf("val = %d\n", tmp2->top->value);
-//         tmp2->top = tmp2->top->next;
-//     }
-//     return (0);
-// }
--- srcs/sort/sort_5.c
+++ srcs/sort/sort_5.c
@@ -6,7 +6,46 @@
 /*   By: kdg <kdg@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/23 17:48:11 by kdg               #+#    #+#             */
-/*   Updated: 2026/03/23 17:48:16 by kdg              ###   ########.fr       */
+/*   Updated: 2026/03/23 18:49:27 by kdg              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "push_swap.h"
+
+/*
+	1. find 0,1(ra,rra) and push to b (pb)
+	2. sort 3 in a (sort_3)
+	3. push back (pa,pa)
+*/
+
+static void find_01(t_stack *a, t_stack *b, int target_index)
+{
+	int	i;
+	int	size;
+
+	size = a->size;
+	i = 0;
+	while (a->top->index != target_index)
+	{
+		if (i < size / 2)
+			ra(a);
+		else
+			rra(a);
+		i++;
+	}
+	pb(a, b);
+}
+
+void	sort_5(t_stack *a, t_stack *b)
+{
+	// int	size;
+
+	// size = a->size;
+	find_01(a, b, 0);
+	find_01(a, b, 1);
+	sort_3(a);
+	if (b->top->index < b->top->next->index)
+		sb(b);
+	pa(a, b);
+	pa(a, b);
+}
